c48ff664a17e258b0066b13af4d5db3976111945
Petr Kireev <realkireev@yandex.ru>
2023-05-03 11:45:57 +0300

    style: fix style errors

--- src/main/java/ru/practicum/shareit/user/storage/UserStorage.java
+++ src/main/java/ru/practicum/shareit/user/storage/UserStorage.java
@@ -5,15 +5,15 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface UserStorage {
-    User create (User user);
+    User create(User user);
 
-    User update (User user, Long userId);
+    User update(User user, Long userId);
 
-    void delete (Long userId);
+    void delete(Long userId);
 
-    Optional<User> findById (Long userId);
+    Optional<User> findById(Long userId);
 
-    Optional<User> findByEmail (String email);
+    Optional<User> findByEmail(String email);
 
-    Collection<User> findAll ();
+    Collection<User> findAll();
 }
